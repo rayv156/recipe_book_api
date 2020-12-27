f20543ffb5328a514cb7398bfa6eff1fc1fda82d
Ray Velasquez <rayvelasquez@Rays-MacBook-Pro.local>
2020-12-26 21:29:17 -0600

    reset production database

--- db/migrate/20201217205724_create_users.rb
+++ db/migrate/20201217205724_create_users.rb
@@ -1,13 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :username
+      t.string :username, unique: true
       t.string :password_digest
       t.string :email
       t.integer :age
 
       t.timestamps
     end
-    add_index :users, :username, unique: true
   end
 end
